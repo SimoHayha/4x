35b98da7ea69ded941cade04b04630630a725bb1
v.delporte <delporte.valentin@gmail.com>
2015-06-16 21:40:23 +0200

    fix library

---
+++ Assets/Scripts/Game/Logic/Universe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Universe : MonoBehaviour
+{
+    private int turn = 0;
+
+    public void OnTurn()
+    {
+        turn++;
+#if DEBUG
+        GameObject.FindGameObjectWithTag("GUIDebugTurn").GetComponent<Text>().text = "Turn " + turn;
+#endif
+    }
+}
